4ba8eccc35a9995f358a4d7d9caa2a35faa093a3
yinhao <yinhao@jd.com>
2018-09-07 17:47:21 +0800

    7-1 Vue项目首页 header区域开发 部分

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <div class="header-left">左侧</div>
+    <div class="header-input">输入</div>
+    <div class="header-right">右侧</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style>
+
+</style>
