bf912a41890be2d6db2fb9235f6e1bf6f40a0f13
yinhao-mac <yinhao@jd.com>
2018-09-07 21:39:27 +0800

    7-2 Vue项目首页 - iconfont 的使用和代码优化

--- src/main.js
+++ src/main.js
@@ -4,14 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-// 让页面在每个终端显示的内容一致
-import './assets/styles/reset.css'
+// webpack.base.conf.js 中 设置别名 styles 代表 src/assets/styles目录 让页面在每个终端显示的内容一致
+import 'styles/reset.css'
 
 // 解决移动端1像素边框问题
-import './assets/styles/border.css'
+import 'styles/border.css'
 
 // 解决移动端点击延迟300ms问题，需要引入依赖
 import fastClick from 'fastclick'
+
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
