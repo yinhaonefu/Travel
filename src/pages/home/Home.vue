89e82370e0df877e7dcb43fed8b180f401478f82
yinhao-mac <yinhao@jd.com>
2018-09-07 21:08:23 +0800

    7-1 Vue项目首页 - header区域开发

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -6,16 +6,15 @@
 
 <script>
 import HomeHeader from './components/Header'
+
 export default {
   name: 'Home',
   components: {
     HomeHeader
   }
 }
+
 </script>
 
 <style>
-.home{
-  font-size: 50px;
-}
 </style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <div class="header-left">左侧</div>
-    <div class="header-input">输入</div>
-    <div class="header-right">右侧</div>
+    <div class="header-left">返回</div>
+    <div class="header-input">输入城市/景点/游玩主题</div>
+    <div class="header-right">城市</div>
   </div>
 </template>
 
@@ -12,6 +12,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+.header
+  display: flex
+  line-height: .86rem
+  background: #00bcd4
+  color: #fff
+  .header-left
+    width: .64rem
+    float: left
+  .header-input
+    flex: 1
+    height .64rem
+    line-height .64rem
+    margin-top .12rem
+    margin-left .2rem
+    background #fff
+    border-radius .1rem
+    color #ccc
+  .header-right
+    width: 1.24rem
+    float: right
+    text-align: center
 </style>
