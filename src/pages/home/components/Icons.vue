6fce8cbbb02eb4a6d876b27a19e94a5c339d6aea
yinhao-mac <yinhao@jd.com>
2018-09-08 12:42:07 +0800

    7-4 Vue项目首页 - 图标区域页面布局

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="icons">
+    <div class="icon" v-for="item of iconList" :key="item.id">
+      <div class="icon-img">
+        <img class="icon-img-content" :src="item.iconUrl" />
+      </div>
+      <p class="icon-desc">{{item.desc}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: 1,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点'
+        },
+        {
+          id: 2,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        },
+        {
+          id: 3,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png',
+          desc: '夏日玩水'
+        },
+        {
+          id: 4,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          desc: '文化古迹'
+        },
+        {
+          id: 5,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+          desc: '动植物园'
+        },
+        {
+          id: 6,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+          desc: '故宫'
+        },
+        {
+          id: 7,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '一日游'
+        },
+        {
+          id: 8,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1808/8d/747c9a29b8dba402.png',
+          desc: '国宝巡礼'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.icons
+  overflow hidden
+  width: 100%
+  height: 0
+  padding-bottom 50%
+  .icon
+    position relative
+    width 25%
+    padding-bottom 25%
+    float left
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom .4rem
+      box-sizing border-box
+      padding .1rem
+      .icon-img-content
+        display block
+        margin 0 auto
+        height 100%
+    .icon-desc
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height .4rem
+      line-height .4rem
+      text-align center
+      color $darkTextColor
+</style>
