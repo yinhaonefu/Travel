661d09166d4bbec1787f77c8c54309889e989d9a
yinhao-mac <yinhao@jd.com>
2018-09-09 17:05:27 +0800

    8-2 Vue项目城市选择页 - 搜索框布局

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="city">
-      <city-header></city-header>
-    </div>
+  <div class="city">
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
